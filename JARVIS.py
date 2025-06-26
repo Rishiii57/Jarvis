b658ea96d91d4832a91c26d19e9ed4ef8e5ed44d
akshit-codes <chhabra.akshit@gmail.com>
2025-06-26 21:31:24 +0530

    demo

--- JARVIS.py
+++ JARVIS.py
@@ -4,6 +4,7 @@ import pyttsx3
 import musiclib
 import requests
 from dotenv import dotenv_values
+import country_converter as coco
 
 secrets = dotenv_values(".env")
 
@@ -20,63 +21,95 @@ def speak(text):
 
 def process(c):
     c = c.lower()
-    if "open google" in c:
-        webbrowser.open("https://google.com")
-    elif "open youtube" in c:
-        webbrowser.open("https://youtube.com")
-    elif "open facebook" in c:
-        webbrowser.open("https://facebook.com")
-    elif "open instagram" in c:
-        webbrowser.open("https://instagram.com")
-    elif "open twitter" in c or "open x" in c:
-        webbrowser.open("https://twitter.com")
-    elif "open gmail" in c:
-        webbrowser.open("https://mail.google.com")
-    elif "open reddit" in c:
-        webbrowser.open("https://reddit.com")
-    elif "open stackoverflow" in c:
-        webbrowser.open("https://stackoverflow.com")
-    elif "open github" in c:
-        webbrowser.open("https://github.com")
-    elif "open whatsapp" in c:
-        webbrowser.open("https://web.whatsapp.com")
-    elif "open netflix" in c:
-        webbrowser.open("https://netflix.com")
-    elif "open hotstar" in c:
-        webbrowser.open("https://hotstar.com")
-    elif "open amazon" in c:
-        webbrowser.open("https://amazon.in")
-    elif "open flipkart" in c:
-        webbrowser.open("https://flipkart.com")
-    elif c.lower().startswith("play "):
-        song = c[5:]  # remove "play " and take the rest as song name
-        if song in musiclib.music:
-            webbrowser.open(musiclib.music[song])
-            speak(f"Playing {song}")
-        else:
-            speak("Sorry, I don't know that song.")
-    elif "news" in c.lower():
-        print("ok news")
-        url = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={newsapi}"
-        response = requests.get(url)
-        data = response.json()
+    command = c[:5]
+    if (command=="open"):
+        open(c)
+    elif (command=="play"):
+        play(c)
+    elif (command=="give"):
+        give(c)
+    else:
+        speak("Unknown Command")
 
-        if data["status"] == "ok":
-            speak("Here are top three newsheadlines")
-            articles = data["articles"][:3]  
-            for article in articles:
-                speak(article["title"])
+aliases = {
+    "g": "google",
+    "yt": "youtube",
+    "fb": "facebook",
+    "ig": "instagram",
+    "x": "twitter",
+    "twt": "twitter",
+    "mail": "gmail",
+    "r": "reddit",
+    "so": "stackoverflow",
+    "gh": "github",
+    "nf": "netflix",
+    "hs": "hotstar",
+    "fk": "flipkart",
+    "az": "amazon.in",
+    "amazon": "amazon.in",
+    "wp": "web.whatsapp.com",
+    "whatsapp": "web.whatsapp.com",
+}
+
+def open(s):
+    s = s[5:]
+    url = aliases.get(url, url)
+    if '.' in url:
+        if '://' in url:
+            webbrowser.open(url)
         else:
-            speak("Breaking news: There is no breaking news.")
-    elif("weather" in c.lower()):
-        print("ok weather")
-        url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={weatherapi}&units=metric"
-        response = requests.get(url)
-        data = response.json()
-        speak(f"Temperature {data['main']['temp']} degree celcius ")
-        speak(f"Weather {data['weather'][0]['description']}")
-        speak(f"Humidity {data['main']['humidity']}%")
-        speak(f"Wind Speed {data['wind']['speed']} meter per second")
+            webbrowser.open("https://" + url)
+    else:
+        webbrowser.open("https://" + url + ".com")
+
+def play(s):
+    song = s[5:]
+    if song in musiclib.music:
+        webbrowser.open(musiclib.music[song])
+        speak(f"Playing {song}!")
+    else:
+        speak("Sorry, I don't know that song.")
+
+def give(s):
+    if 'news' in s:
+        country = 'INDIA'
+        if len(s) > 8:
+            country = s[8:]
+        news(country)
+    elif 'weather' in s:
+        weather()
+    else:
+        speak("Invalid give command.")
+
+def news(country):
+    print("ok news")
+    
+    country_code = coco.convert(country, to="ISO2").lower()
+    country_code = (country_code, 'in')[country_code=='not found']
+    url = f"https://newsapi.org/v2/top-headlines?country={country_code}&apiKey={newsapi}"
+    
+    response = requests.get(url)
+    data = response.json()
+    
+    if data["status"] == "ok":
+        speak("Here are top three news headlines.")
+        articles = data["articles"][:3]  
+        for article in articles:
+            speak(article["title"])
+    else:
+        speak("Breaking news: There is no breaking news.")
+
+def weather():
+    print("ok weather")
+    
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={weatherapi}&units=metric"
+    response = requests.get(url)
+    data = response.json()
+    
+    speak(f"Temperature {data['main']['temp']} degree celcius ")
+    speak(f"Weather {data['weather'][0]['description']}")
+    speak(f"Humidity {data['main']['humidity']}%")
+    speak(f"Wind Speed {data['wind']['speed']} meter per second")
 
 if __name__ == "__main__":
     speak("Initializing jarvis")
